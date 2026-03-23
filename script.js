9032628059595a86b461c32ed7ba5c3a7bb790d9
Jackson Vega <jacksonvega94@gmail.com>
2026-03-22 20:59:03 -0500

    segunda version

---
+++ script.js
@@ -0,0 +1,49 @@
+ const toggle = document.getElementById("menu-toggle");
+    const nav = document.getElementById("nav");
+    const overlay = document.getElementById("overlay");
+
+    toggle.addEventListener("click", () => {
+        toggle.classList.toggle("active");
+        nav.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    overlay.addEventListener("click", () => {
+        toggle.classList.remove("active");
+        nav.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+    // Cerrar al hacer click en links
+    document.querySelectorAll("#nav a").forEach(link => {
+        link.addEventListener("click", () => {
+            toggle.classList.remove("active");
+            nav.classList.remove("active");
+            overlay.classList.remove("active");
+        });
+    });
+    // LIGHTBOX
+    const imagenes = document.querySelectorAll("#galeria img");
+    const lightbox = document.getElementById("lightbox");
+    const imgGrande = document.getElementById("img-grande");
+    const cerrar = document.getElementById("cerrar");
+
+    // Abrir imagen
+    imagenes.forEach(img => {
+        img.addEventListener("click", () => {
+            lightbox.style.display = "flex";
+            imgGrande.src = img.src;
+        });
+    });
+
+    // Cerrar con X
+    cerrar.addEventListener("click", () => {
+        lightbox.style.display = "none";
+    });
+
+    // Cerrar haciendo click afuera
+    lightbox.addEventListener("click", (e) => {
+        if (e.target !== imgGrande) {
+            lightbox.style.display = "none";
+        }
+    });
